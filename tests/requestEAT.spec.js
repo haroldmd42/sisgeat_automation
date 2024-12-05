3d9956cc0c0854330d18833295103111859231c3
haroldmd42 <yanmd4266@gmail.com>
2024-12-05 12:40:03 -0500

    Time out

--- tests/requestEAT.spec.js
+++ tests/requestEAT.spec.js
@@ -3,6 +3,6 @@ import { requestEAT } from '../object/requestEAT.js';
 
 //Radicar una nueva solicitud
 test('Request EAT form', async ({ page }) => {
-    
+test.slow()    
 await requestEAT.formRequestEAT(page)
 });
